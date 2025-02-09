f0ab4afa3de5043bd7e7ee1aa726ea4faf0d6b23
ujval10 <ujvalsharma591@gmail.com>
2025-02-09 20:45:46 +0530

    Commit

---
+++ e-commerce/src/redux/actions/contains/reducers/productReducer.js
@@ -0,0 +1,18 @@
+import { ActionTypes } from "../action-types"
+
+const initialState = {
+    products:[{
+        id: 1,
+        title:"ujval",
+        category:"programmer",
+    },],
+};
+export const productReducer =(state = initialState, {type, payload})=>{
+    switch (type){
+        case ActionTypes.SET_PRODUCTS:
+            return state;
+        default:
+            return state;
+    }
+
+};
