2dd24904083a5f2f2f19446e69a04304cc04ee92
ujval10 <ujvalsharma591@gmail.com>
2025-02-15 10:48:20 +0530

    Commit

--- e-commerce/src/containers/ProductComponent.js
+++ e-commerce/src/containers/ProductComponent.js
@@ -1,28 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const ProductComponent = () => {
     const products = useSelector((state) => state.allProducts.products);
 
     return (
-        <div className="four column wide">
-            <div className="ui link cards">
+        <div className="ui three column grid container">
+            <div className="row">
                 {products && products.length > 0 ? (
-                    products.map((product) => {
-                        const { id, title, image, price, category } = product;
-                        return (
-                            <div className="card" key={id}>
-                                <div className="image">
-                                    <img src={image} alt={title} />
-                                </div>
-                                <div className="content">
-                                    <div className="header">{title}</div>
-                                    <div className="meta price">$ {price}</div>
-                                    <div className="meta">{category}</div>
-                                </div>
+                    products.map((product) => (
+                        <div className="column" key={product.id}>
+                            <div className="ui card" style={{ minHeight: "350px" }}>
+                                <Link to={`/product/${product.id}`}>
+                                    <div className="image">
+                                        <img 
+                                            src={product.image} 
+                                            alt={product.title} 
+                                            style={{ maxHeight: "200px", objectFit: "contain" }} 
+                                        />
+                                    </div>
+                                    <div 
+                                        className="content" 
+                                        style={{ 
+                                            whiteSpace: "normal", 
+                                            overflow: "hidden", 
+                                            textOverflow: "ellipsis", 
+                                            minHeight: "100px" 
+                                        }}
+                                    >
+                                        <div className="header">{product.title}</div>
+                                        <div className="meta price">$ {product.price}</div>
+                                        <div className="meta">{product.category}</div>
+                                    </div>
+                                </Link>
                             </div>
-                        );
-                    })
+                        </div>
+                    ))
                 ) : (
                     <h2>Loading...</h2>
                 )}
--- e-commerce/src/containers/ProductDetail.js
+++ e-commerce/src/containers/ProductDetail.js
@@ -1,7 +1,48 @@
-import React from "react"
+import React, { useEffect, useCallback } from "react";
+import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { selectedProduct } from "../redux/actions/productActions";
+import "./ProductDetail.css"; // Import CSS for styling
 
-const ProductDetail = () =>{
-    return(<div><h1>ProductDetail</h1></div>)
-}
+const ProductDetail = () => {
+    const { productId } = useParams();
+    const dispatch = useDispatch();
+    const product = useSelector((state) => state.product);
 
-export default ProductDetail
+    const fetchProductDetail = useCallback(async () => {
+        try {
+            const response = await axios.get(`https://fakestoreapi.com/products/${productId}`);
+            dispatch(selectedProduct(response.data));
+        } catch (err) {
+            console.error("Error fetching product details:", err);
+        }
+    }, [productId, dispatch]);
+
+    useEffect(() => {
+        if (productId) {
+            fetchProductDetail();
+        }
+    }, [productId, fetchProductDetail]);
+
+    if (!product || !product.title) {
+        return <h2>Loading...</h2>;
+    }
+
+    return (
+        <div className="product-container">
+            <div className="image-container">
+                <img src={product.image} alt={product.title} />
+            </div>
+            <div className="details-container">
+                <h2>{product.title}</h2>
+                <div className="price-tag">${product.price}</div>
+                <p className="category">men clothing</p>
+                <p>{product.description}</p>
+                <button className="add-to-cart">Add to Cart</button>
+            </div>
+        </div>
+    );
+};
+
+export default ProductDetail;
